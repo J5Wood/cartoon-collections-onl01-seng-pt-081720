28491dbc1d24a1c74edc965f6136e14b5f694a43
Jeremy Wood <js.wood206@gmail.com>
2020-08-26 06:27:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planet_powers)
-  planet_powers.collect { |power| power.uppercase + "!"}
+  planet_powers.collect { |power| power.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
