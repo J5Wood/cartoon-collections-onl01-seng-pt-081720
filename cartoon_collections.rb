8fafb9fa568bbaa4b2ed9fa7d4990fb37d65d297
Jeremy Wood <js.wood206@gmail.com>
2020-08-26 06:25:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index { |x, i| "#{i + 1} " + x }
+  dwarves.each_with_index { |name, i| "#{i + 1} " + name }
 end
 
 def summon_captain_planet# code an argument here
