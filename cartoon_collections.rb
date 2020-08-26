b966f5a0ef4443d8ea0138b1001f1aa11fdab155
Jeremy Wood <js.wood206@gmail.com>
2020-08-26 06:38:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,12 @@ def long_planeteer_calls(long_calls)
   long_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(grocery_bag)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+    if grocery_bag.include?(cheese_types[i])
+      return
+    else
+      i += 1
 end
